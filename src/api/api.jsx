3672d270d7e34101dbda20d935eee75825aec86f
Prasanna0401 <prasannavenkatesh2002@gmail.com>
2025-03-17 15:44:13 +0530

    first commit

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '3bb18026';
-const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+const BASE_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
 export const searchMovies = async (query, page = 1) => {
   const response = await axios.get(`${BASE_URL}&s=${query}&page=${page}`);
